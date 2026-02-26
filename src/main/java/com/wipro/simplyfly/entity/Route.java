9997b6274444f34b52580bdd9ca78dbc08420e76
Kaavya <kaavya446@gmail.com>
2026-02-26 15:32:16 +0530

    implemented mapping and added route repository

--- src/main/java/com/wipro/simplyfly/entity/Route.java
+++ src/main/java/com/wipro/simplyfly/entity/Route.java
@@ -1,10 +1,14 @@
 package com.wipro.simplyfly.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Route {
@@ -21,19 +25,26 @@ public class Route {
     private Double distance;
 
     private String estimatedDuration;
+    
+    @OneToMany(mappedBy = "route", cascade = CascadeType.ALL)
+    private List<Flight> flights;
 
 	public Route() {
 		super();
 	}
 
-	public Route(int id, String source, String destination, Double distance, String estimatedDuration) {
+	
+	public Route(int id, String source, String destination, Double distance, String estimatedDuration,
+			List<Flight> flights) {
 		super();
 		this.id = id;
 		this.source = source;
 		this.destination = destination;
 		this.distance = distance;
 		this.estimatedDuration = estimatedDuration;
+		this.flights = flights;
 	}
+
 
 	public int getId() {
 		return id;
@@ -75,11 +86,18 @@ public class Route {
 		this.estimatedDuration = estimatedDuration;
 	}
 
+	public List<Flight> getFlights() {
+		return flights;
+	}
+
+	public void setFlights(List<Flight> flights) {
+		this.flights = flights;
+	}
+	
 	@Override
 	public String toString() {
 		return "Route [id=" + id + ", source=" + source + ", destination=" + destination + ", distance=" + distance
 				+ ", estimatedDuration=" + estimatedDuration + "]";
 	}
-    
     
 }
