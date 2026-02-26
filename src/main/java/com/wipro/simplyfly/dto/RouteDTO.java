91ca28c12428bd78553a6fd5557133e24b239759
Kaavya <kaavya446@gmail.com>
2026-02-26 11:25:46 +0530

    created route entity and dto

---
+++ src/main/java/com/wipro/simplyfly/dto/RouteDTO.java
@@ -0,0 +1,70 @@
+package com.wipro.simplyfly.dto;
+
+import jakarta.persistence.Column;
+
+public class RouteDTO {
+	
+	    private int id;
+
+	    private String source;
+
+	    private String destination;
+
+	    private Double distance;
+
+	    private String estimatedDuration;
+
+		public RouteDTO() {
+			super();
+		}
+
+		public RouteDTO(int id, String source, String destination, Double distance, String estimatedDuration) {
+			super();
+			this.id = id;
+			this.source = source;
+			this.destination = destination;
+			this.distance = distance;
+			this.estimatedDuration = estimatedDuration;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getSource() {
+			return source;
+		}
+
+		public void setSource(String source) {
+			this.source = source;
+		}
+
+		public String getDestination() {
+			return destination;
+		}
+
+		public void setDestination(String destination) {
+			this.destination = destination;
+		}
+
+		public Double getDistance() {
+			return distance;
+		}
+
+		public void setDistance(Double distance) {
+			this.distance = distance;
+		}
+
+		public String getEstimatedDuration() {
+			return estimatedDuration;
+		}
+
+		public void setEstimatedDuration(String estimatedDuration) {
+			this.estimatedDuration = estimatedDuration;
+		}
+
+}
