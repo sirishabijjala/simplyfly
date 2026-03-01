c38f9829d1c0ffb34ce6b639e4bdfc84d1639f52
Sai Prasanna <thotasaiprasanna77@gmail.com>
2026-03-01 17:25:40 +0530

    adding rest controller

--- src/main/java/com/wipro/simplyfly/dto/BookingRequestDTO.java
+++ src/main/java/com/wipro/simplyfly/dto/BookingRequestDTO.java
@@ -1,9 +1,17 @@
 package com.wipro.simplyfly.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+
 public class BookingRequestDTO {
+@Null(message="UserID cannot be null")
 private Long userId;
+@Null(message="ScheduleId cannot be null")
 private Long ScheduleId;
+@Min(value=1,message="Atleast one seat has to select for booking")
 private int numberOfSeats;
+@NotNull(message="Payment method is required")
 private String paymentMethod;//CARD,UPI,NETBANKING
 public BookingRequestDTO() {
 	super();
