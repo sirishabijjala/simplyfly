5322c9b88c7e1fe88db41eb027ae9ab9606b123a
sirishabijjala <sirishabijjala356@gmail.com>
2026-03-02 09:52:05 +0530

    updated

--- src/main/java/com/wipro/simplyfly/config/SecurityConfig.java
+++ src/main/java/com/wipro/simplyfly/config/SecurityConfig.java
@@ -32,10 +32,9 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/register", "/auth/login")
+                .requestMatchers("/auth/register", "/auth/login", "/api/admin/**")
                 .permitAll()
-                .requestMatchers("/api/admin/**")
-                .hasAuthority("ADMIN")
+                
                 .anyRequest()
                 .authenticated()
             )
