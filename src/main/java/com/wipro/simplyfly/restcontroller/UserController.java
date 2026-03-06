52a281c8bc6112ec2142e063469970c425ec0479
Januser687 <Jan1user687@rpsconsulting.in>
2026-03-06 12:39:01 +0530

    Resolved stash conflict in UserController

--- src/main/java/com/wipro/simplyfly/restcontroller/UserController.java
+++ src/main/java/com/wipro/simplyfly/restcontroller/UserController.java
@@ -14,7 +14,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 
-
 @SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/users")
