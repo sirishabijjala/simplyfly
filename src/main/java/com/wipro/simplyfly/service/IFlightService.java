bde8934f64d1de9e545fb58d0a2eaed55f5324f7
Kaavya <kaavya446@gmail.com>
2026-03-06 11:49:14 +0530

    seat selection ui

--- src/main/java/com/wipro/simplyfly/service/IFlightService.java
+++ src/main/java/com/wipro/simplyfly/service/IFlightService.java
@@ -2,10 +2,13 @@ package com.wipro.simplyfly.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import com.wipro.simplyfly.dto.FlightSearchResponseDTO;
 
 public interface IFlightService  {
 
 	public List<FlightSearchResponseDTO> searchFlights(String from, String to, LocalDate date);
+
+	List<Map<String, Object>> getSeatsBySchedule(Long scheduleId);
 }
